0cd29093a790a9acb8a636c8ab64ab69263734f3
stoneHz <stonehz@gmail.com>
2013-05-28 12:51:15 +0100

    testing rolify n+1 issue

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RolifyTut::Application.config.secret_token = '44dcb708f91e8790bb5bc58c0407a284f8439a9c9ca769b488dc2a74efbe7bab28eebb160faca77f9b3b33b52b59154ab5f0edf32a5180cfd10e2510e65bf2a4'
